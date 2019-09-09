4b32f5812c3561afd238dc04cd62c4fad2ce5437
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-09-09 13:42:05 -0400

    pulling env vars into node app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const express = require('express')
-const server = express()
-const port = 3000
-server.get('/', (req, res) => res.send('Hello world'))
-server.listen(port, () => console.log(`listening on ${port}`))
+require("dotenv").config();
+
+const { Pool, Client } = require("pg");
+
+const pool = new Pool();
+
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
+
+const express = require("express");
+const server = express();
+const port = process.NODE_APP_ENV || 3000;
+
+server.get("/", (req, res) => res.send("Hello world"));
+server.listen(port, () => console.log(`listening on ${port}`));
