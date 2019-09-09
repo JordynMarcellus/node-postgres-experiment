64c662162d517f4c7666a7ce58db9b3a419e3118
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-09-09 17:55:29 -0400

    Modified WIP commit

--- src/index.js
+++ src/index.js
@@ -5,17 +5,36 @@ require("dotenv").config({
 });
 const { Pool } = require("pg");
 
+let pool = null;
 console.log(process.env.DB_CONNECTION_STRING);
+while (pool === null) {
+  try {
+    const tempPool = new Pool({
+      connectionString: process.env.DB_CONNECTION_STRING,
+    });
+    const query = tempPool.query("SELECT NOW()", (err, res) => {
+      console.log(err, res);
+      pool.end();
+      if (err) return err;
+      console.log(res);
+      return res;
+    });
+    pool = tempPool;
+    break;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-const pool = new Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-});
+// const pool = new Pool({
+//   connectionString: process.env.DB_CONNECTION_STRING,
+// });
 
-pool.query("SELECT NOW()", (err, res) => {
-  console.log(err, res);
-  pool.end();
-  if (err) return process.exit(1);
-});
+// pool.query("SELECT NOW()", (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+//   if (err) return process.exit(1);
+// });
 
 const express = require("express");
 const server = express();
