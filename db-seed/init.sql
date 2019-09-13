195b787e90c00956c722983aaf19414d3a1fc628
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-09-13 18:27:11 -0400

    Adds new extension to db and actually seeds the data with a single value

--- db-seed/init.sql
+++ db-seed/init.sql
@@ -1,4 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS players(
-  player_id serial PRIMARY KEY,
-  player_name varchar(45) NOT NULL
-)
+  player_id uuid DEFAULT uuid_generate_v4(),
+  player_name varchar(45) NOT NULL,
+  PRIMARY KEY (player_id)
+);
+
+INSERT INTO players (player_name) VALUES ('Connor McDavid');
