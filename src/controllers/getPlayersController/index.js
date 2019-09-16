7fa8399fc6ef33baf0225ce93d577b981767f947
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-09-16 16:04:31 -0400

    First unit test && re-factored controller code

--- src/controllers/getPlayersController/index.js
+++ src/controllers/getPlayersController/index.js
@@ -1,24 +1,30 @@
 const { dbConnector } = require("../../db/db");
 
-exports.getPlayersController = (req, res) => {
-  dbConnector.connect((err, client, release) => {
-    // when we add error handling we can poke at this more
-    if (err) {
-      console.error("error connecting client", err.stack);
-      return res.status(500).send();
-    }
-
-    client.query("SELECT * from public.players", (err, data) => {
-      // db pool requires a release, run it right after query has been made to minimize connections according to docs.
-      release();
-      if (err) {
-        console.error("error executing query", err.stack);
-        return res.status(500).send();
-      }
-      const rowsOfData = data.rows;
-      rowsOfData.length === 0
-        ? res.status(204).send()
-        : res.status(200).send(rowsOfData);
-    });
-  });
+exports.getPlayersController = async (req, res) => {
+  try {
+    const { rows } = await dbConnector
+      .query("SELECT * from public.players")
+      .then(res => res)
+      .catch(e => {
+        throw e;
+      });
+    return res.status(200).send(rows);
+  } catch (e) {
+    console.error("error executing query", e.stack);
+    return res.status(500).send();
+  }
+  // const dbResponse = dbConnector.query("SELECT * from public.players") (err, data) => {
+  //   console.log("wave");
+  //   console.log;
+  //   // db pool requires a release, run it right after query has been made to minimize connections according to docs.
+  //   release();
+  //   if (err) {
+  //     console.error("error executing query", err.stack);
+  //     return res.status(500).send();
+  //   }
+  //   const rowsOfData = data.rows;
+  //   rowsOfData.length === 0
+  //     ? res.status(204).send()
+  //     : res.status(200).send(rowsOfData);
+  // });
 };
---
+++ src/controllers/getPlayersController/index.test.js
@@ -0,0 +1,30 @@
+const { getPlayersController } = require("./");
+const { dbConnector } = require("../../db/db");
+
+const mockRow = {
+  rows: [
+    {
+      player_id: "54bba2f0-e09b-40c6-a0ec-99c06c2e639e",
+      player_name: "Connor McDavid",
+    },
+  ],
+};
+
+jest.mock("../../db/db.js");
+
+describe("getPlayersController", () => {
+  afterEach(() => jest.clearAllMocks());
+  it("actually works? ", async () => {
+    dbConnector.query = jest.fn().mockResolvedValue(mockRow);
+    const mockRes = {
+      send: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+    };
+    const mockReq = {};
+    const calledFunction = await getPlayersController(mockReq, mockRes);
+    expect(dbConnector.query).toHaveBeenCalled();
+    expect(dbConnector.query).toHaveBeenCalledWith(
+      "SELECT * from public.players"
+    );
+  });
+});
