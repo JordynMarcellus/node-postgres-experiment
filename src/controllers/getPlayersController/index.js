1b857deb1218bfce8dcc9499952e5ba89b0a18a5
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-09-16 14:16:14 -0400

    Puts helmet in app as a middleware, adds temp routes to act as a guide for app development

--- src/controllers/getPlayersController/index.js
+++ src/controllers/getPlayersController/index.js
@@ -4,18 +4,18 @@ exports.getPlayersController = (req, res) => {
   dbConnector.connect((err, client, release) => {
     // when we add error handling we can poke at this more
     if (err) {
-      return console.error("error connecting client", err.stack);
+      console.error("error connecting client", err.stack);
+      return res.status(500).send();
     }
 
     client.query("SELECT * from public.players", (err, data) => {
+      // db pool requires a release, run it right after query has been made to minimize connections according to docs.
       release();
       if (err) {
-        //
         console.error("error executing query", err.stack);
-        return res.status(400).send();
+        return res.status(500).send();
       }
       const rowsOfData = data.rows;
-      console.log(rowsOfData);
       rowsOfData.length === 0
         ? res.status(204).send()
         : res.status(200).send(rowsOfData);
--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ const { getPlayersController } = require("./controllers/getPlayersController");
 const server = express();
 const port = process.env.NODE_APP_PORT || 3000;
 
-server.get("/", (req, res) => res.send("Hello!!!"));
+const tempCallback = (req, res) =>
+  res.status(204).send("please excuse the mess!!!");
+// need to
 server.get("/players", getPlayersController);
+// update player; indicating a player has been selected
+server.patch("/players", tempCallback);
+// create new player
+server.post("/players", tempCallback);
+server.get("/players/:playerId/details", tempCallback);
+server.get("*", (req, res) => res.status(400).send());
+
 server.listen(port, () => console.log(`listening on ${port}!!!`));
