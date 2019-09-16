fb37224d380e5cca24cbcdb68895e3d9715b6a1c
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-09-16 12:09:24 -0400

    Scraper now successfully grabs all table rows

--- scrapers/corsica-player-rankings.js
+++ scrapers/corsica-player-rankings.js
@@ -3,22 +3,39 @@ const URL =
   "https://www.corsicahockey.com/nhl/players/nhl-player-ratings-rankings";
 const WAIT_OPTIONS = { waitUntil: "networkidle2" };
 
+const grabTextContent = node => node.grabTextContent;
+
+const PLAYER_NAME = 0;
+const PLAYER_POSITION = 2;
+const PLAYER_RATING = 3;
+const PLAYER_RANKING = 4;
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
 
   try {
     const browserPage = await browser.newPage();
     await browserPage.goto(URL);
+    await browserPage.click('a[data-position="forwards"]');
+    await browserPage.waitFor(1000 * 10);
     await browserPage.select("select#filter_limits_select", "4000");
-    await browserPage.click('a[data-position="skaters"]');
-    await browserPage.waitFor(1000 * 60);
-    // double bang here is to explicitly coerce to true or false...
-    // potentially using this here in case we need to load the data
-    // await page.waitFor(() => !!document.querySelector('.foo'))
+    // i am being lazy with my scraper - ten seconds to load data then we'll traverse the tables rows.
+    await browserPage.waitFor(1000 * 10);
+    const tableRows = await browserPage.$$(
+      ".player-ratings-table.frozen-table tbody > tr"
+    );
+    const players = await Promise.all(
+      tableRows.map(async element => {
+        const playerName = await element.$$eval("td", rowOfTableCells =>
+          rowOfTableCells.map(grabTextContent)
+        );
+        return playerName;
+      })
+    );
   } catch (e) {
     console.error("shutting down due to", e.message);
     console.error(e.stack);
   } finally {
+    console.log("closing...");
     browser.close();
     // should we process.exit here?
   }
