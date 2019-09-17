7aa63590959b1a707efd36cae2062c6f9190fd64
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-09-16 20:16:38 -0400

    Successfully imports a csv into the database on startup

--- db-seed/init.sql
+++ db-seed/init.sql
@@ -5,10 +5,10 @@ CREATE TABLE IF NOT EXISTS players(
   name varchar(45) NOT NULL,
   selected boolean DEFAULT false,
   position varchar(4) NOT NULL,
-  team varchar(4) NOT NULL,
+  team varchar(50) NOT NULL,
   rank smallint NOT NULL,
   rating real NOT NULL
-
 );
 
-INSERT INTO players (name, position, team, rank, rating) VALUES ('Connor McDavid', 'C', 'EDM', 1, 89.34 );
+COPY public.players(name, position, team, rank, rating)
+FROM '/docker-entrypoint-initdb.d/test.csv' DELIMITER ',' CSV HEADER;
