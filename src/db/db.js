f1136295cc6cc513c41e15de7d8886a222a19302
Jordyn Marcellus <jordynmarcellus@gmail.com>
2020-06-04 13:31:01 -0400

    Cooking with gas in gitignored files

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,14 @@
 const { Pool } = require("pg");
-const dbPool = new Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-});
+const dbPool = new Pool();
 
 // this could be a heartbeat/health-check endpoint?
 dbPool.connect((err, client, release) => {
   if (err) {
     console.log("client connection issue", err.stack);
+  } else {
+    console.log("successful connection");
+    release();
   }
-  console.log("successful connection");
-  release();
 });
 
 exports.dbConnector = dbPool;
